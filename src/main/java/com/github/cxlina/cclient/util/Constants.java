846f8a981f6b1ad4a69668cde09ed3f9a10a57a7
Celina <wittmarcel13@gmail.com>
2022-06-21 13:03:37 +0200

    Added Comment.

--- src/main/java/com/github/cxlina/cclient/util/Constants.java
+++ src/main/java/com/github/cxlina/cclient/util/Constants.java
@@ -4,5 +4,5 @@ import net.fabricmc.loader.impl.FabricLoaderImpl;
 
 public class Constants {
 
-    public static String CLIENT_VERSION = "1.0.0_" + (FabricLoaderImpl.INSTANCE.isDevelopmentEnvironment() ? "development" : "release");
+    public static String CLIENT_VERSION = "1.1.0_" + (FabricLoaderImpl.INSTANCE.isDevelopmentEnvironment() ? "development" : "release");
 }
